8440ee2c9b9a5e6b68b4398255d186c2a0ed98c1
Sue Mniszewski <smm@lanl.gov>
2023-10-24 14:11:29 -0600

    Added leap versions.

---
+++ algorithm/2-clustering/quantum_graph_2clustering_leap.py
@@ -0,0 +1,121 @@
+import numpy as np
+import numpy.linalg as la
+import networkx as nx
+
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite, FixedEmbeddingComposite
+from dimod.reference.samplers import ExactSolver
+import minorminer
+
+from scipy.io import mmread
+import matplotlib.pyplot as plt
+import argparse
+import logging
+import os
+import sys
+import random
+import urllib3
+
+import graph_2ClusterAlgorithm_functions as QCD
+import graphFileUtility_functions as GFU
+
+
+# The Quantum Graph Community Detection Algorithm has been described
+# in the following publications. Please cite in your publication.
+#
+# H. Ushijima-Mwesigwa, C. F. A. Negre, S. M. Mniszewski,
+# 2017, Graph Partitioning using Quantum Annealing on the
+# D-Wave System, Proceedings of the 2nd International
+# Workshop on Post Moore’s Era Supercomputing (PMES), 22-29.
+#
+# C. F. A. Negre, H. Ushijima-Mwesigwa, S. M. Mniszewski 2020, Detecting
+# Multiple Communities using Quantum Annealing on the D-Wave System,
+# PLOS ONE 15(2): e0227538. https://doi.org/10.1371/journal.pone.0227538
+#
+# S. M. Mniszewski, P. A. Dub, S. Tretiak, P. M. Anisimov, Y. Zhang,
+# C. F. A. Negre, 2021, Reduction of the Molecular Hamiltonian Matrix using 
+# Quantum Community Detection, Sci Rep 11, 4099 (2021).
+# https://doi.org/10.1038/s41598-021-83561-x#
+#
+
+if __name__== '__main__':
+
+  urllib3.disable_warnings()
+
+  parser = argparse.ArgumentParser(description='Quantum Community Detection 2-clustering - Leap hybrid workflow')
+  parser.add_argument('-ifile', help='input filename')
+  parser.add_argument('-ftype', default='umtx', help='input file type (mtx, umtx, 0mtx, mi)')
+  parser.add_argument('-pflag', type=int, default=0, help='plot flag, 0-no 1-yes')
+  parser.add_argument('-nparts', type=int, default=2, help='number of parts')
+  parser.add_argument('-label', default='q2cd_leapHybrid', help='label for run')
+  parser.add_argument('-myprofile', default='', help='D-Wave profile for run')
+  parser.add_argument('-time_limit', type=int, default=0, help='time limit in seconds')
+
+  args = parser.parse_args()
+
+  print('input file  = ', args.ifile)
+  print('file type = ', args.ftype)
+  print('plot flag = ', args.pflag)
+  print('number parts = ', args.nparts)
+  print('label = ', args.label)
+  print('myprofile = ', args.myprofile)
+  print('time_limit = ', args.time_limit)
+
+  ifile = args.ifile
+  ftype = args.ftype
+  pflag = args.pflag
+  nparts = args.nparts
+  run_label = args.label
+  run_profile = args.myprofile
+  tlimit = args.time_limit
+ 
+  # Read in file as graph
+  threshold = 0.0
+  graph = GFU.createGraph(ftype, ifile, threshold) 
+  
+  num_nodes = nx.number_of_nodes(graph)
+  num_edges = nx.number_of_edges(graph)
+  print ("\n\t community detection: up to %d communities...\n" %nparts)
+  print ("Graph has %d nodes and %d edges" %(num_nodes, num_edges))
+
+  # Collect results to dictionary
+  result = {}
+  result['alg'] = 'LANL_2CD'
+  result['num_parts'] = nparts
+  result['dataset'] = ifile
+  result['nodes'] = num_nodes
+  result['edges'] = num_edges
+  result['size'] = num_nodes
+  result['solver'] = 'DWAVE_LEAPHYBRID'
+
+  # Create Adjacency matrix A
+  A = nx.adjacency_matrix(graph)
+  print ('\nAdjacency matrix:\n', A.todense())
+
+  # Calculate Modularity matrix B
+  mtotal,B = QCD.buildMod(A, threshold)
+  print('\nModularity matrix:\n', B)
+
+  # Cluster into 2 parts
+  ss = QCD.clusterLeapHybrid(B, num_nodes, nparts, run_label, run_profile, tlimit, result)
+
+  # Process solution
+  part_number,cdet = QCD.process_solution(ss, num_nodes)
+
+  # Calculate modularity meetric
+  mmetric = QCD.calcModularityMetric(mtotal, B, part_number)
+  print('\nmodularity metric = ', mmetric)
+  result['modularity_metric'] = mmetric
+  mcut = QCD.calc_cut(graph, part_number)
+  print('\nMin cut = ', mcut)
+  result['minimum_cut'] = mcut
+  result['num_clusters_found'] = nparts
+  print(flush=True)
+
+  GFU.write_resultFile(result)
+
+  # Show plot of parts if requested
+  if pflag == 1:
+    GFU.showClusters(part_number, graph)
+
+  exit(0)
---
+++ algorithm/2-partitioning/quantum_graph_2partitioning_leap.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import re, os, sys
+import numpy as np
+
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import FixedEmbeddingComposite
+import minorminer
+
+import networkx as nx
+from numpy import linalg as la
+from networkx.generators.atlas import *
+import random, copy
+import math
+import argparse
+from scipy.sparse import csr_matrix
+import urllib3
+
+import graph_2PartitionAlgorithm_functions as QGP
+import graphFileUtility_functions as GFU
+
+#
+# The Quantum Graph Partitioning Algorithm has been described
+# in the following publications. Please cite in your publication.
+#
+# H. Ushijima-Mwesigwa, C. F. A. Negre, S. M. Mniszewski,
+# 2017, Graph Partitioning using Quantum Annealing on the
+# D-Wave System, Proceedings of the 2nd International
+# Workshop on Post Moore’s Era Supercomputing (PMES), 22-29.
+#
+
+
+if __name__== '__main__':
+
+  urllib3.disable_warnings()
+
+  parser = argparse.ArgumentParser(description='Quantum Graph 2-Partitioning - Leap hybrid workflow')
+  parser.add_argument('-nparts', type=int, default=2, help='number of parts')
+  parser.add_argument('-pflag', type =int, default=0, help='0 - no plot, 1 - show plot')
+  parser.add_argument('-ifile', help='input filename in mtx format')
+  parser.add_argument('-ftype', default='umtx', help='file type: mtx, 0mtx, umtx')
+  parser.add_argument('-beta', type=int, default=1, help='beta penalty constant: minimize edge cut')
+  parser.add_argument('-alpha', type=int, default=1000, help='alpha penalty constant: balancing')
+  parser.add_argument('-gamma', type=int, default=5000, help='gamma penalty constant: each node in 1 part')
+  parser.add_argument('-label', default='q2gp_leapHybrid', help='label for run')
+  parser.add_argument('-myprofile', default='', help='D-Wave profile  for run')
+  parser.add_argument('-time_limit', type=int, default=0, help='time limit in seconds')
+
+  args = parser.parse_args()
+ 
+  print('number parts = ', args.nparts)
+  print('plot flag = ', args.pflag)
+  print('mtx file = ', args.ifile)
+  print('file type = ', args.ftype)
+  print('beta = ', args.beta)
+  print('alpha = ', args.alpha)
+  print('gamma = ', args.gamma)
+  print('label = ', args.label)
+  print('myprofile = ', args.myprofile)
+  print('time_limit = ', args.time_limit)
+
+  print('\n')
+
+  num_parts = args.nparts
+  pflag = args.pflag
+  ifilename = args.ifile
+  ftype = args.ftype
+  beta0 = args.beta
+  alpha0 = args.alpha
+  gamma0 = args.gamma
+  run_label = args.label
+  run_profile = args.myprofile
+  tlimit = args.time_limit
+
+  ####
+  # NOTE: node and matrix indexing starts from 0
+  #
+  ###
+
+  # Read in graph from mtx file
+  threshold = 0.0
+  graph = GFU.createGraph(ftype, ifilename, threshold)
+
+  num_parts = 2
+  num_nodes = nx.number_of_nodes(graph)
+  num_edges = nx.number_of_edges(graph)
+  print("\n\t Partitioning into %d parts...\n" %num_parts)
+  print("Graph has %d nodes and %d edges" %(num_nodes, nx.number_of_edges(graph)))
+  print(flush=True)
+
+  # Collect results to dictionary
+  result = {}
+  result['alg'] = 'LANL_2GP'
+  result['num_parts'] = num_parts
+  result['dataset'] = ifilename
+  result['nodes'] = num_nodes
+  result['edges'] = num_edges
+  result['size'] = num_nodes
+  result['solver'] = 'DWAVE_LEAPHYBRID'
+
+  # Set penalty constants
+  alpha = alpha0
+  beta = beta0
+  gamma = gamma0
+
+  # Create QUBO matrix 
+  laplacian = nx.laplacian_matrix(graph)
+  Q = QGP.makeQubo(laplacian, alpha, beta, gamma, graph, num_nodes, num_parts)
+  print('QUBO created')
+  print(flush=True)
+  
+  # Run 2-partitioning with D-Wave Leap hybrid workflow using ocean
+  ss = QGP.partitionLeapHybrid(Q, num_nodes, num_parts, run_label, run_profile, tlimit, result)
+  print('Partitioning done')
+  print(flush=True)
+
+  # Process solution
+  part_number,cdet = QGP.process_solution(ss, graph, num_nodes, num_parts, result)
+  print('Postprocessing done')
+  print(flush=True)
+
+  GFU.write_partfile(graph, part_number, num_nodes, num_parts)
+
+  # Get results and compare to other tools (if available)
+  min_cut = QGP.compare_with_metis_and_kahip(graph, part_number, num_nodes, num_parts, result)
+  result['min_cut_metric'] = min_cut
+
+  GFU.write_resultFile(result)
+
+  # Show plot of parts if requested
+  if pflag == 1:
+    GFU.show_partitions(graph, part_number)
